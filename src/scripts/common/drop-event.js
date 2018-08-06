8f7e948b28cbf83078d87552ceefcbf790872c53
Voloshin Konstantin <23.03@i.ua>
2018-08-07 00:22:24 +0300

    responsive start

--- src/scripts/common/drop-event.js
+++ src/scripts/common/drop-event.js
@@ -2,22 +2,24 @@ const $ = require('jquery');
 
 $(document).ready(function() {
     if($('.drop-down').length) {
-        
-        $('.menu__item').mousemove(function(){
-            $(this).addClass('active');
-            $('.drop-down', this).css('left', '0');
-            $('.drop-down', this).css('z-index', '1');
-            if($(window).scroll(function(){
-                $('.menu__item').removeClass('active');
-                $('.drop-down').css('left', '9999px');
-                $('.drop-down').css('z-index', '-1');
-            }));
-        });
-        
-        $('.menu__item').mouseout(function(){
-            $(this).removeClass('active');
-            $('.drop-down', this).css('left', '9999px');
-            $('.drop-down', this).css('z-index', '-1');
-        });
+        if(document.documentElement.clientWidth > 769) {
+            $('.menu__item').mousemove(function(){
+                $(this).addClass('active');
+                $('.drop-down', this).css('left', '0');
+                $('.drop-down', this).css('z-index', '1');
+
+                if($(window).scroll(function(){
+                    $('.menu__item').removeClass('active');
+                    $('.drop-down').css('left', '9999px');
+                    $('.drop-down').css('z-index', '-1');
+                }));
+            });
+
+            $('.menu__item').mouseout(function(){
+                $(this).removeClass('active');
+                $('.drop-down', this).css('left', '9999px');
+                $('.drop-down', this).css('z-index', '-1');
+            });
+        }
     }
 })
