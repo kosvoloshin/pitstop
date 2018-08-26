12995f2fb2e458adf1ebe825b7def0352cffcb66
Voloshin Konstantin <23.03@i.ua>
2018-08-26 13:21:56 +0300

    responcive contacts, about, events

--- src/scripts/common/calendar.js
+++ src/scripts/common/calendar.js
@@ -3,20 +3,28 @@ const $ = require('jquery');
 $(document).ready(function() {
     if($('.calendar__btn-event').length){
       $("document").ready(function($){
-                
-        $('.calendar__btn-event').on('click', function(event) {
-            event.preventDefault();
-            $('.calendar__btn-event').toggleClass('active');
-            $('.calendar__row--hiden').toggleClass('active');
-        });
+               
+            $('.calendar__btn-event').on('click', function(event) {
+                event.preventDefault();
+                $('.calendar__btn-event').toggleClass('active');
+                $('.calendar__row--hiden').toggleClass('active');
+                $('.tooltip').removeClass('scale');
+            });
+        if(document.documentElement.clientWidth > 769) { 
+            $('.calendar__date.active').mousemove(function(){
+                $('.tooltip').css('transform', 'scale(1)');
+            });
 
-        $('.calendar__date.active').mousemove(function(){
-            $('.tooltip').css('transform', 'scale(1)');
-        });
+            $('.calendar__date.active').mouseout(function(){
+                $('.tooltip').css('transform', 'scale(0)');
+            });
+        };
 
-        $('.calendar__date.active').mouseout(function(){
-            $('.tooltip').css('transform', 'scale(0)');
-        });
+        if(document.documentElement.clientWidth < 769) { 
+            $('.calendar__date.active').on('click', function(){
+                $('.tooltip').toggleClass('scale');
+            });
+        };
 
       });
     }
