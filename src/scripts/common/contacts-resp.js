c4a9c171e3e343c0428a2250fc5c6291d183d4f4
Voloshin Konstantin <23.03@i.ua>
2018-11-14 00:04:09 +0200

    first test commit

--- src/scripts/common/contacts-resp.js
+++
@@ -1,9 +0,0 @@
-const $ = require('jquery');
-
-$(document).ready(function() {
-    if($('#append').length){
-        if(document.documentElement.clientWidth < 992) {
-            $('#two').detach().prependTo('#one');
-        };
-    };
-});
