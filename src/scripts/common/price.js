c9679f84e7204ed129684fc300d4a9c09f10a80d
Voloshin Konstantin <23.03@i.ua>
2018-11-20 23:46:49 +0200

    coding progress

--- src/scripts/common/price.js
+++ src/scripts/common/price.js
@@ -100,4 +100,44 @@ $(document).ready(function() {
             
         });
     };
+
+    if($('.tabs').length){
+        $(document).on("scroll", onScroll);
+        
+        //smoothscroll
+        $('a[href^="#"].tabs__link').on('click', function (e) {
+            e.preventDefault();
+            $(document).off("scroll");
+            
+            $('a').each(function () {
+                $(this).removeClass('active');
+            })
+            $(this).addClass('active');
+            
+            var target = this.hash,
+                menu = target;
+            $target = $(target);
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top+2
+            }, 500, 'swing', function () {
+                window.location.hash = target;
+                $(document).on("scroll", onScroll);
+            });
+        });
+        
+        function onScroll(event){
+            var scrollPos = $(document).scrollTop();
+            $('.tabs a').each(function () {
+                var currLink = $(this);
+                var refElement = $(currLink.attr("href"));
+                if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                    $('.tabs ul li a').removeClass("active");
+                    currLink.addClass("active");
+                }
+                else{
+                    currLink.removeClass("active");
+                }
+            });
+        }
+    };
 });
