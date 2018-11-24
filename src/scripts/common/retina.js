4601b3d8f1f8f290ede99deb2096ec2190acddae
Voloshin Konstantin <23.03@i.ua>
2018-11-24 16:02:42 +0200

    coding progress

--- src/scripts/common/retina.js
+++ src/scripts/common/retina.js
@@ -1,13 +1,21 @@
 const $ = require('jquery');
 
-$(document).ready(function() {
-    var images = $('.service__item img');
-    images.each(function(i) {
-        $(this).width($(this).width() / 2);
+function retina() {
+    $(document).ready(function() {
+        if($('.service__list').length){
+            var images = $('.service__item img');
+            images.each(function(i) {
+                $(this).width($(this).width() / 2);
+            });
+        };
+        
+        if($('.service-tabs__list').length){
+            var images2 = $('.service-tabs__item img');
+            images2.each(function(i) {
+                $(this).width($(this).width() / 2);
+            });
+        };
     });
+};
 
-    var images2 = $('.service-tabs__item img');
-    images2.each(function(i) {
-        $(this).width($(this).width() / 2);
-    });
-});
+window.onload = retina;
